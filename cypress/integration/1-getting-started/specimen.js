38f54ffd011800147dbc9c7a301ed46b7e3ed016
Binod Chowdhury <pnl03e41@Emea.Royalahold.net>
2022-04-20 12:16:03 +0200

    First commit

---
+++ cypress/integration/1-getting-started/specimen.js
@@ -0,0 +1,23 @@
+const dummy = require('dummynator');
+const fullname = dummy.person.firstName() + ' ' + dummy.person.lastName()
+describe('My first test case', function(){
+    it('Open the page playground',function(){
+        cy.visit('https://dineshvelhal.github.io/testautomation-playground/forms.html')
+                     
+    })
+
+    it('Enter the experience',function(){
+        cy.get('#exp').type(dummy.random.number())
+    })
+    
+    it('Enter other details',function(){
+        cy.get('#check_python').check()
+        cy.get('#notes').type(fullname)
+        cy.get('#upload_cv').click()
+    })
+
+    it('Upload a pic',function(){
+        cy.upload_validate('Paw1.PNG')
+    })
+
+})
