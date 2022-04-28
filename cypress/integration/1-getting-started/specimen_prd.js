3ad6be1a47f7573ab90ab9643510f8796cb43fa1
Binod Chowdhury <pnl03e41@Emea.Royalahold.net>
2022-04-29 00:02:05 +0200

    Added Multiple Env run and screenshot file name based on env

--- cypress/integration/1-getting-started/specimen_prd.js
+++ cypress/integration/1-getting-started/specimen_prd.js
@@ -1,10 +1,10 @@
 const dummy = require('dummynator');
 const fullname = dummy.person.firstName() + ' ' + dummy.person.lastName()
 
-describe('My first test case', function(){
+describe('My First test case for Prod', function(){
     it('Open the page & take screenshot',function(){
         
-        cy.Open_page_screenshot('testdata')
+        cy.Open_page_screenshot('testdataprd')
         cy.screenshots_datetime_format('Initial_screenshot')
                      
     })
@@ -14,21 +14,21 @@ describe('My first test case', function(){
     })
     
     it('Enter other details',function(){
-        cy.get('#check_python').check()
+        cy.get('#check_javascript').check()
  //    cy.get('#notes').type(fullname)
         cy.get('#notes').type(Cypress.env('Notes'),{log: false})
         cy.get('#upload_cv').click()
     })
 
     it('Upload a pic',function(){
-        cy.upload_validate('Paw1.PNG')
+        cy.upload_validate('Paw2.PNG')
     })
 
 })
 
 describe('Choose the language',()=>{
     it('language selected', ()=>{
-        cy.choose_language('testdata') 
+        cy.choose_language('testdataprd') 
         cy.screenshots_datetime_format('Choose_lang') 
          
     })
